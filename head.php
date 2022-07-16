d45a39b284a1744b0804093ec989081298c09679
fkadeal <96823972+F21deal@users.noreply.github.com>
2022-07-16 04:13:41 -0800

    distributed

--- head.php
+++ head.php
@@ -9,10 +9,13 @@
 <head>
 	<title> ገበታ </title>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	<script src="https://getbootstrap.com/docs/5.0/dist/js/bootstrap.bundle.min.js" ></script>
+	<script src="https://getbootstrap.com/docs/5.0/dist/js/bootstrap.bundle.min.js" ></script> 
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <script src="order.js"></script>
+
 	<script src="../../web/js/bootstrap.bundle.min.js" ></script>
-	<link rel="stylesheet" href="../../web/css/bootstrap.css" type="text/css" >
-	<link rel="stylesheet" href="../../web/css/styleWA.css" type="text/css" >
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" type="text/css" >
+	<link rel="stylesheet" href="sign.css" type="text/css" >
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
--- products.php
+++ products.php
@@ -23,21 +23,44 @@ include "head.php";
 			
 			echo '<div class="col img_tmp" style="border-radius: 30px;">'; 
 			echo '<div class="cardii" >'; 
-            echo '<form method="post" action="cart_update.php">';
+            echo '<form method="GET" action="cart_update.php">';
 			echo '<img class="center-cropped myDIV" alt="'.$obj->productName.'" src="images/'.$obj->Picture.'">';
             echo '<div class="p_name hide"><b>'.$obj->productName.'</b>';
+			echo '<input type="hidden" name="productName" value="'.$obj->productName.'" />';
 			echo '<input type="hidden" name="Product_ID" value="'.$obj->Product_ID.'" />';
             echo '<input type="hidden" name="type" value="add" />';
             echo '</form>';
             echo '</div>';
 			echo '</div>';
-			echo '</div>';
-        }
+			echo '</div>'; 
+			
+            echo "<script type='text/JavaScript'> 
+            // Make a request for a user with a given ID
+            function sendOrder () { 
+           axios.get('http://192.168.8.167:5000/?order_id=12121&user_id=34435&total_cost=234535&items=document.getElementsByName('productName').values')
+             .then(function (response) {
+               // handle success
+               console.log(response);
+             })
+             .catch(function (error) {
+               // handle error
+               console.log(error)
+             })
+             .then(function () {
+               // always executed
+             });
+           }
+           
+                </script>";
+
+                echo "<button onclick='sendOrder()'>Ordering</button>"; 
+        
+            }
+            }
     
-    }
 	?>
 </div>
 </div>
 <?php
-include "footer.php";
+include "footer.php";	
 ?>
